edc1935481a3e7504cd31e6b76a2d148c962b26a
Пользователь <ionmax1311@gmail.com>
2020-08-08 18:59:45 +0300

    add project

--- build/precache-manifest.81398fdc56a511f273da9ee03b6283e2.js
+++ build/precache-manifest.81398fdc56a511f273da9ee03b6283e2.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "104cecdc30367df9cc8d0ccfbc02954e",
+    "revision": "9f644091b14aaddb8edb253365cf1595",
     "url": "/index.html"
   },
   {
-    "revision": "14e7250ac62117e2d8f5",
-    "url": "/static/css/main.00c09063.chunk.css"
+    "revision": "d7665d0f0384e8ac07c9",
+    "url": "/static/css/main.b6ec1186.chunk.css"
   },
   {
     "revision": "987f7621fc23afe575e5",
     "url": "/static/js/2.83972604.chunk.js"
   },
   {
-    "revision": "14e7250ac62117e2d8f5",
-    "url": "/static/js/main.ad123075.chunk.js"
+    "revision": "d7665d0f0384e8ac07c9",
+    "url": "/static/js/main.eea50fc9.chunk.js"
   },
   {
     "revision": "d37e5e5e9932e30ff6cf",
